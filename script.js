fa366625633a38fb5c9df3e4eb3068d91cd83280
GAETESI <133069009+GAETESI@users.noreply.github.com>
2023-05-26 22:13:54 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("page loaded...");
+var video = document.querySelector("video");
+
+function startPreview(){
+    video.muted = true,
+    video.currentTime = 1;
+    video.playbackRate = 0.5;
+    video.play();
+}
+
+function stopPreview(){
+    video.currentTime = 0;
+    video.playbackRate = 0.5;
+    video.pause();
+}
+var previewTimeout = null;
+
+video.addEventListener("mouseenter", () => {
+    startPreview();
+    previewTimeout = setTimeout(stopPreview, 4000);
+});
+
+video.addEventListener("mouseleave", () => {
+    clearTimeout(previewTimeout);
+    previewTimeout = null;
+    stopPreview();
+});
